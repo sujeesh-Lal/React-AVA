63e0084cda8670c97870621a4a162216f343b137
Sujeesh Lal <sujeesh-Lal@users.noreply.github.com>
2017-05-30 19:14:57 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,24 @@
 const test = require('ava')
-// Need to add test cases 
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import ReactDOMServer from 'react-dom-server';
+import App from './../src/App';
+
 test('Can get queue length', t => {
   t.pass();
   console.log('success')
+});
+
+test('FieldError shows when the error is in array', assert => {
+  const errors = [{ attribute: 'email', code: '123' }];
+  const div = document.createElement('div');
+  const field = React.createElement(App, {
+  }, 'Error message');
+  ReactDOM.render(field, div);
+  assert.pass();
+  // const result = ReactDOMServer.renderToStaticMarkup(field);
+  console.log(result)
+  // assert.same(result, '<span class="form__error">Error message</span>');
 });
 
 test('foo', t => {
